1a48a8183d951fed66b22356f90f50daa658b725
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2019-11-15 17:37:40 -0300

    Add classe Produto

---
+++ Rico.LojaVirtua.Dominio/Entidades/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rico.LojaVirtua.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public string Categoria { get; set; }
+
+    }
+}
